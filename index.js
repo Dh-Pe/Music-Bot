2fdae05f4b5be5606c0154cf52ae82cc6e70bbd9
JovemHer <69544085+JovemHer@users.noreply.github.com>
2021-11-03 18:36:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+//Packges
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = "SEU TOKEN AQUI";
+const fs = require('fs');
+client.queue = new Map();
+client.vote = new Map();
 
-client.on('ready', async () => {
-  console.log(`${client.user} está online!`)
-})
+//Handler para eventos
 
-client.on(token)
+var Eventos = fs.readdirSync('./events')
+Eventos.forEach(f => {
+const Empty = f.split('.')[0]
+const None = require(`../events/${f}`)
+client.on(Empty, None.bind(null, client))
+});
+
+//Ligando o Bot
+
+client.login(token)
